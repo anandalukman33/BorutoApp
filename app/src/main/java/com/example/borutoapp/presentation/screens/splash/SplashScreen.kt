b71496ed9fbd339aa44a9dc0cd1d006fa0e2683e
Ananda Muhamad Lukman <88027328+anandalukman33@users.noreply.github.com>
2023-09-26 23:26:04 +0700

    Animated Icon App When Splash Shown

--- app/src/main/java/com/example/borutoapp/presentation/screens/splash/SplashScreen.kt
+++ app/src/main/java/com/example/borutoapp/presentation/screens/splash/SplashScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -19,19 +20,28 @@ import com.example.borutoapp.R
 import com.example.borutoapp.ui.theme.Purple500
 import com.example.borutoapp.ui.theme.Purple700
 import com.example.borutoapp.ui.theme.ShimmerDarkGray
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.draw.rotate
 
 @Composable
 fun SplashScreen(navHostController: NavHostController) {
-    Splash()
+    val degrees = remember { Animatable(0f) }
+    LaunchedEffect(key1 = true) {
+        degrees.animateTo(targetValue = 360f, animationSpec = tween(durationMillis = 2000, delayMillis = 200))
+    }
+    Splash(degrees = degrees.value)
 }
 
 @Composable
-fun Splash() {
+fun Splash(degrees: Float) {
     if (isSystemInDarkTheme()) {
         Box(modifier = Modifier
             .background(Brush.verticalGradient(listOf(Color.Black, ShimmerDarkGray)))
             .fillMaxSize(), contentAlignment = Alignment.Center) {
             Image(
+                modifier = Modifier.rotate(degrees = degrees),
                 painter = painterResource(id = R.drawable.ic_logo),
                 contentDescription = stringResource(R.string.boruto_app_logo)
             )
@@ -41,6 +51,7 @@ fun Splash() {
             .background(Brush.verticalGradient(listOf(Purple700, Purple500)))
             .fillMaxSize(), contentAlignment = Alignment.Center) {
             Image(
+                modifier = Modifier.rotate(degrees = degrees),
                 painter = painterResource(id = R.drawable.ic_logo),
                 contentDescription = stringResource(R.string.boruto_app_logo)
             )
@@ -52,11 +63,11 @@ fun Splash() {
 @Composable
 @Preview
 fun SplashScreenPreview() {
-    Splash()
+    Splash(degrees = 0f)
 }
 
 @Composable
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
 fun SplashScreenDarkPreview() {
-    Splash()
+    Splash(degrees = 0f)
 }
