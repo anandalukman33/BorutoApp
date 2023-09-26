010252b15be47ad1d839b86e760bf1ef014a3cac
Ananda Muhamad Lukman <88027328+anandalukman33@users.noreply.github.com>
2023-09-27 00:20:57 +0700

    Design Welcome Screen Part 3 (Create Indicator Pager)

--- app/src/main/java/com/example/borutoapp/presentation/screens/welcome/WelcomeScreen.kt
+++ app/src/main/java/com/example/borutoapp/presentation/screens/welcome/WelcomeScreen.kt
@@ -1,17 +1,23 @@
 package com.example.borutoapp.presentation.screens.welcome
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -21,14 +27,22 @@ import androidx.navigation.NavHostController
 import com.example.borutoapp.R
 import com.example.borutoapp.domain.model.OnBoardingPage
 import com.example.borutoapp.ui.theme.EXTRA_LARGE_PADDING
+import com.example.borutoapp.ui.theme.PAGING_INDICATOR_SPACING
+import com.example.borutoapp.ui.theme.PAGING_INDICATOR_WIDTH
 import com.example.borutoapp.ui.theme.SMALL_PADDING
+import com.example.borutoapp.ui.theme.activeIndicatorColor
+import com.example.borutoapp.ui.theme.buttonBackgroundColor
 import com.example.borutoapp.ui.theme.descriptionColor
+import com.example.borutoapp.ui.theme.inactiveIndicatorColor
 import com.example.borutoapp.ui.theme.titleColor
 import com.example.borutoapp.ui.theme.welcomeScreenBackgroundColor
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.HorizontalPagerIndicator
+import com.google.accompanist.pager.PagerState
 import com.google.accompanist.pager.rememberPagerState
 
+@ExperimentalAnimationApi
 @ExperimentalPagerApi
 @Composable
 fun WelcomeScreen(navHostController: NavHostController) {
@@ -42,11 +56,27 @@ fun WelcomeScreen(navHostController: NavHostController) {
             .background(MaterialTheme.colors.welcomeScreenBackgroundColor)
     ) {
         HorizontalPager(
+            modifier = Modifier.weight(10f),
             count = page.size,
             state = pagerState,
             verticalAlignment = Alignment.Top
         ) {
             PagerScreen(onBoardingPage = page[it])
+        }
+
+        HorizontalPagerIndicator(
+            modifier = Modifier
+                .weight(1f)
+                .align(Alignment.CenterHorizontally),
+            pagerState = pagerState,
+            activeColor = MaterialTheme.colors.activeIndicatorColor,
+            inactiveColor = MaterialTheme.colors.inactiveIndicatorColor,
+            indicatorWidth = PAGING_INDICATOR_WIDTH,
+            spacing = PAGING_INDICATOR_SPACING
+        )
+        
+        FinishButton(pagerState = pagerState, modifier = Modifier.weight(1f)) {
+            
         }
     }
 }
@@ -61,7 +91,9 @@ fun PagerScreen(onBoardingPage: OnBoardingPage) {
     ) {
         
         Image(
-            modifier = Modifier.fillMaxWidth(0.5f).fillMaxSize(0.7f),
+            modifier = Modifier
+                .fillMaxWidth(0.5f)
+                .fillMaxSize(0.7f),
             painter = painterResource(id = onBoardingPage.image),
             contentDescription = stringResource(R.string.on_boarding_image)
         )
@@ -92,6 +124,39 @@ fun PagerScreen(onBoardingPage: OnBoardingPage) {
         
     }
 
+}
+
+@ExperimentalAnimationApi
+@ExperimentalPagerApi
+@Composable
+fun FinishButton(
+    pagerState: PagerState,
+    modifier: Modifier,
+    onClick: () -> Unit
+
+) {
+    Row(
+        modifier = modifier.padding(horizontal = EXTRA_LARGE_PADDING),
+        verticalAlignment = Alignment.Top,
+        horizontalArrangement = Arrangement.Center
+
+    ) {
+        AnimatedVisibility(
+            visible = pagerState.currentPage == 2,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Button(
+                onClick = onClick,
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = MaterialTheme.colors.buttonBackgroundColor,
+                    contentColor = Color.White
+                )
+            ) {
+                Text(text = "Finish")
+            }
+        }
+
+    }
 }
 
 @Composable
