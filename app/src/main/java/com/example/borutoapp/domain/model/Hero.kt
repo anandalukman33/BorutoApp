b640df4cc943ed73e172eba1154c208216ebc442
Ananda Muhamad Lukman <88027328+anandalukman33@users.noreply.github.com>
2023-09-27 10:58:20 +0700

    Create Api Response and Boruto Api

--- app/src/main/java/com/example/borutoapp/domain/model/Hero.kt
+++ app/src/main/java/com/example/borutoapp/domain/model/Hero.kt
@@ -3,7 +3,9 @@ package com.example.borutoapp.domain.model
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.example.borutoapp.util.Constants
+import kotlinx.serialization.Serializable
 
+@Serializable
 @Entity(tableName = Constants.HERO_DATABASE_TABLE)
 data class Hero(
 
