92aa68d77eb784b0f37b11f5e51818df3316658e
Ananda Muhamad Lukman <88027328+anandalukman33@users.noreply.github.com>
2023-09-27 10:50:20 +0700

    Create HomeScreen Top Bar

--- app/src/main/java/com/example/borutoapp/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/borutoapp/presentation/screens/home/HomeScreen.kt
@@ -1,9 +1,21 @@
 package com.example.borutoapp.presentation.screens.home
 
+import android.annotation.SuppressLint
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
+import com.example.borutoapp.presentation.screens.home.componen.topbar.HomeScreenTopBar
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun HomeScreen(
     navHostController: NavHostController
-) {}
+) {
+    Scaffold(
+        topBar = {
+            HomeScreenTopBar(onSearchClicked = {})
+        }
+    ) { _ ->
+
+    }
+}
---
+++ app/src/main/java/com/example/borutoapp/presentation/screens/home/componen/topbar/HomeScreenTopBar.kt
@@ -0,0 +1,49 @@
+package com.example.borutoapp.presentation.screens.home.componen.topbar
+
+import android.content.res.Configuration
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.borutoapp.R
+import com.example.borutoapp.ui.theme.topAppBarBackgroundColor
+import com.example.borutoapp.ui.theme.topAppBarContentColor
+
+@Composable
+fun HomeScreenTopBar(onSearchClicked: () -> Unit) {
+    TopAppBar(
+        title = {
+            Text(text = "Explore")
+        },
+        contentColor = MaterialTheme.colors.topAppBarContentColor,
+        backgroundColor = MaterialTheme.colors.topAppBarBackgroundColor,
+        actions = {
+            IconButton(onClick = onSearchClicked) {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = stringResource(R.string.search_icon)
+                )
+            }
+        }
+    )
+}
+
+@Composable
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_NO)
+fun HomeTopBarLightPreview() {
+    HomeScreenTopBar {}
+}
+
+@Composable
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+fun HomeTopBarDarkPreview() {
+    HomeScreenTopBar {
+
+    }
+}
