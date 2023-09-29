feb11bea988f2465223a6cd15950c0f3ae3b54f5
Ananda Muhamad Lukman <88027328+anandalukman33@users.noreply.github.com>
2023-09-29 14:14:21 +0700

    Design and Create Logic about Rating Widget

--- app/src/main/java/com/example/borutoapp/presentation/attribute/RatingWidget.kt
+++ app/src/main/java/com/example/borutoapp/presentation/attribute/RatingWidget.kt
@@ -1,9 +1,15 @@
 package com.example.borutoapp.presentation.attribute
 
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.geometry.Size
@@ -14,39 +20,80 @@ import androidx.compose.ui.graphics.drawscope.translate
 import androidx.compose.ui.graphics.vector.PathParser
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.borutoapp.R
+import com.example.borutoapp.ui.theme.EXTRA_SMALL_PADDING
 import com.example.borutoapp.ui.theme.LightGray
 import com.example.borutoapp.ui.theme.StarColor
+import timber.log.Timber
 
 @Composable
 fun RatingWidget(
     modifier: Modifier,
     rating: Double,
-    scaleFactor: Float = 3f
+    scaleFactor: Float = 3f,
+    spaceBetween: Dp = EXTRA_SMALL_PADDING
 ) {
-
+    val result = calculateStars(rating = rating)
     val starPathString = stringResource(id = R.string.star_path)
     val starPath = remember {
-        PathParser().parsePathString(pathData = starPathString).toPath()
+        PathParser()
+            .parsePathString(pathData = starPathString)
+            .toPath()
     }
-
     val starPathBounds = remember {
         starPath.getBounds()
     }
-    
-    FilledStar(
-        starPath = starPath,
-        starPathBounds = starPathBounds,
-        scaleFactor = scaleFactor
-    )
 
+    Row(modifier = modifier, horizontalArrangement = Arrangement.spacedBy(spaceBetween)) {
+
+        /**
+         * Filled Stars Section
+         */
+        result["filledStars"]?.let {
+            repeat(it) {
+                FilledStar(
+                    starPath = starPath,
+                    starPathBounds = starPathBounds,
+                    scaleFactor = scaleFactor
+                )
+            }
+        }
+
+        /**
+         * Half Filled Stars Section
+         */
+        result["halfStars"]?.let {
+            repeat(it) {
+                HalfFilledStar(
+                    starPath = starPath,
+                    starPathBounds = starPathBounds,
+                    scaleFactor = scaleFactor,
+                    rating = rating
+                )
+            }
+        }
+
+        /**
+         * Empty Stars Section
+         */
+        result["emptyStars"]?.let {
+            repeat(it) {
+                EmptyStar(
+                    starPath = starPath,
+                    starPathBounds = starPathBounds,
+                    scaleFactor = scaleFactor
+                )
+            }
+        }
+    }
 }
 
 @Composable
 fun FilledStar(starPath: Path, starPathBounds: Rect, scaleFactor: Float) {
     Canvas(modifier = Modifier.size(24.dp)) {
-        val canvasSize = this.size
+        val canvasSize = size
 
         scale(scale = scaleFactor) {
             val pathWidth = starPathBounds.width
@@ -65,15 +112,31 @@ fun FilledStar(starPath: Path, starPathBounds: Rect, scaleFactor: Float) {
 }
 
 @Composable
-fun HalfFilledStar(starPath: Path, starPathBounds: Rect, scaleFactor: Float) {
+fun HalfFilledStar(starPath: Path, starPathBounds: Rect, scaleFactor: Float, rating: Double) {
     Canvas(modifier = Modifier.size(24.dp)) {
-        val canvasSize = this.size
+        val canvasSize = size
 
         scale(scale = scaleFactor) {
             val pathWidth = starPathBounds.width
             val pathHeight = starPathBounds.height
             val left = (canvasSize.width / 2f) - (pathWidth / 1.7f)
             val top = (canvasSize.height / 2f) - (pathHeight / 1.7f)
+
+            val (_, lastNumber) = rating.toString()
+                .split(".")
+                .map { it.toInt() }
+            val filledCustom: Float = when(lastNumber) {
+                1 -> { 2.1f }
+                2 -> { 2.0f }
+                3 -> { 1.9f }
+                4 -> { 1.8f }
+                5 -> { 1.7f }
+                6 -> { 1.6f }
+                7 -> { 1.5f }
+                8 -> { 1.4f }
+                9 -> { 1.3f }
+                else -> { 1.7f }
+            }
 
             translate(left = left, top = top) {
                 drawPath(
@@ -84,7 +147,7 @@ fun HalfFilledStar(starPath: Path, starPathBounds: Rect, scaleFactor: Float) {
                     drawRect(
                         color = StarColor,
                         size = Size(
-                            width = starPathBounds.maxDimension / 1.7f,
+                            width = starPathBounds.maxDimension / filledCustom, // full stars 0.9 | half = 1.7 | empty = 8.0
                             height = starPathBounds.maxDimension * scaleFactor
                         )
                     )
@@ -94,11 +157,10 @@ fun HalfFilledStar(starPath: Path, starPathBounds: Rect, scaleFactor: Float) {
     }
 }
 
-
 @Composable
 fun EmptyStar(starPath: Path, starPathBounds: Rect, scaleFactor: Float) {
     Canvas(modifier = Modifier.size(24.dp)) {
-        val canvasSize = this.size
+        val canvasSize = size
 
         scale(scale = scaleFactor) {
             val pathWidth = starPathBounds.width
@@ -114,6 +176,41 @@ fun EmptyStar(starPath: Path, starPathBounds: Rect, scaleFactor: Float) {
             }
         }
     }
+}
+
+@Composable
+fun calculateStars(rating: Double): Map<String, Int> {
+    val maxStars by remember { mutableStateOf(5) }
+    var filledStars by remember { mutableStateOf(0) }
+    var halfStars by remember { mutableStateOf(0) }
+    var emptyStars by remember { mutableStateOf(0) }
+
+    LaunchedEffect(key1 = rating) {
+        val (firstNumber, lastNumber) = rating.toString()
+            .split(".")
+            .map { it.toInt() }
+
+        if (firstNumber in 0..5 && lastNumber in 0..9) {
+            filledStars = firstNumber
+            if (lastNumber in 1..9) {
+                halfStars++
+            }
+
+            if (firstNumber == 5 && lastNumber > 0) {
+                emptyStars = 5
+                filledStars = 0
+                halfStars = 0
+            } else {
+                Timber.tag("RatingWidget").d("Invalid Rating Number")
+            }
+        }
+    }
+    emptyStars = maxStars - (filledStars + halfStars)
+    return mapOf(
+        "filledStars" to filledStars,
+        "halfStars" to halfStars,
+        "emptyStars" to emptyStars
+    )
 }
 
 @Composable
@@ -133,7 +230,7 @@ fun HalfFilledStarPreview() {
     val starPathBounds = remember {
         starPath.getBounds()
     }
-    HalfFilledStar(starPath = starPath, starPathBounds = starPathBounds, scaleFactor = 3f)
+    HalfFilledStar(starPath = starPath, starPathBounds = starPathBounds, scaleFactor = 3f, rating = 4.1)
 }
 
 @Preview
--- app/src/main/java/com/example/borutoapp/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/borutoapp/presentation/screens/home/HomeScreen.kt
@@ -1,12 +1,16 @@
 package com.example.borutoapp.presentation.screens.home
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.paging.compose.collectAsLazyPagingItems
+import com.example.borutoapp.presentation.attribute.RatingWidget
 import com.example.borutoapp.presentation.screens.home.component.topbar.HomeScreenTopBar
+import com.example.borutoapp.ui.theme.LARGE_PADDING
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
@@ -20,7 +24,10 @@ fun HomeScreen(
         topBar = {
             HomeScreenTopBar(onSearchClicked = {})
         }
-    ) { _ ->
-
+    ) {
+        RatingWidget(
+            modifier = Modifier.padding(all = LARGE_PADDING),
+            rating = 5.0
+        )
     }
 }
